1d7f24c600d257594790cf133d76a673cf9c8ce6
Kyle Wagner <Keale2@gmail.com>
2016-12-09 10:22:18 -0500

    Add scroll watcher

--- scripts/site.js
+++ scripts/site.js
@@ -1,3 +1,19 @@
-// Main JS should go here!
-// Include scripts using Browserify by doing:
-// var $ = require("jquery");
+let lastScrollPosition = 0;
+let ticking = false;
+
+function update(scrollPosition) {
+    
+}
+
+window.addEventListener('scroll', (e) => {
+    lastScrollPosition = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            update(lastScrollPosition);
+            ticking = false;
+        });
+    }
+
+    ticking = true;
+});
