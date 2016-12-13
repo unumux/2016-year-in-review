9f9b923b1d102ca149a0b8784780afde25959ae2
Addie <azm16@Addies-MacBook-Pro.local>
2016-12-13 16:48:32 -0500

    changesssssss

--- scripts/site.js
+++ scripts/site.js
@@ -17,3 +17,34 @@ window.addEventListener('scroll', (e) => {
 
     ticking = true;
 });
+
+var statsLink = document.querySelector('#stats');
+var projectsLink = document.querySelector('#projects');
+var agileProjectsLink = document.querySelector('#agile-projects-link');
+var emailMarketingLink = document.querySelector('#email-marketing-link');
+var videosLink = document.querySelector('#videos-link');
+var coming2017 = document.querySelector('#coming-2017-link');
+
+statsLink.addEventListener('click', function() {
+    window.location.hash('#stats');
+});
+
+projectsLink.addEventListener('click', function() {
+    window.location.hash('#projects');
+});
+
+agileProjectsLink.addEventListener('click', function() {
+    window.location.hash('#agile-projects');
+});
+
+emailMarketingLink.addEventListener('click', function() {
+    window.location.hash('#email-marketing');
+});
+
+videosLink.addEventListener('click', function() {
+    window.location.hash('#videos');
+});
+
+coming2017.addEventListener('click', function() {
+    window.location.hash('#coming-2017');
+});
